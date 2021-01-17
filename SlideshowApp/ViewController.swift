4f74734b329070af0fe99e4a13b8b1fd36598b40
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-01-17 21:28:39 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by YashimaMasafumi on 2021/01/17.
+//  Copyright © 2021 masa.yashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
