405683773fc72caa9ee7f6735b580c89c0d9ec98
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-01-21 23:38:48 +0900

    画面構成 画像の追加（３枚） 進むボタン⇨画像送り 戻るボタン⇨画像戻り 再生ボタン⇨ボタン名停止に変更、二秒コマ送り

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var img01 :UIImage? = UIImage(named: "img01")
+    var img02 :UIImage? = UIImage(named: "img02")
+    var img03 :UIImage? = UIImage(named: "img03")
+    var imglist :[UIImage]? = []
+    var anime :UIImage?
+    var count :Int = 0
+    
+    @IBOutlet weak var ImageView01: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //self.img01 = UIImage(named: "img01")!
+        //self.img02 = UIImage(named: "img02")!
+        //self.img03 = UIImage(named: "img03")!
+        imglist!.append(img01!)
+        imglist!.append(img02!)
+        imglist!.append(img03!)
+        self.count = 0
     }
+    
+    @IBAction func GoButton(_ sender: Any) {
+        if self.count == imglist!.count - 1 {
+            self.count = 0
+            ImageView01.image = imglist![self.count]
+        }
+        else{
+            self.count += 1
+            ImageView01.image = imglist![self.count]
+        }
+    }
+    
+    @IBAction func BackButton(_ sender: Any) {
+        if self.count == 0{
+            self.count = imglist!.count - 1
+            ImageView01.image = imglist![self.count]
+        }
+        else{
+            self.count -= 1
+            ImageView01.image = imglist![self.count]
+        }
+    }
+    
+    @IBAction func PlayAndStopButton(_ sender: UIButton) {
+        if sender.titleLabel?.text == "再生"{
+            anime = UIImage.animatedImage(with: imglist!, duration: 2)
+            ImageView01.image = anime
+            sender.setTitle("停止", for: UIControl.State.normal)
+        }
+        else if sender.titleLabel?.text == "停止"{
+            ImageView01.image = nil
+            sender.setTitle("再生", for: .normal)
+        }
 
-
+    }
+    
 }
 
