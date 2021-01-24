563c226cc7284461b3fb37597947c0529537ff18
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-01-24 15:56:14 +0900

    timerへの変更 countの修正

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,14 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //picture
     var img01 :UIImage? = UIImage(named: "img01")
     var img02 :UIImage? = UIImage(named: "img02")
     var img03 :UIImage? = UIImage(named: "img03")
     var imglist :[UIImage]? = []
-    var anime :UIImage?
+    //var anime :UIImage?
     var count :Int = 0
-    var animecount :Int = 0
+    var show_count :Int = 0
+    //var animecount :Int = 0
     
+    //timer
+    var timer :Timer!
+    //var timer_sec :Int = 0
+    
+    //UIparts
     @IBOutlet weak var ImageView01: UIImageView!
     @IBOutlet weak var GoButton: UIButton!
     @IBOutlet weak var BackButton: UIButton!
@@ -27,51 +34,47 @@ class ViewController: UIViewController {
         imglist!.append(img01!)
         imglist!.append(img02!)
         imglist!.append(img03!)
-        self.count = 0
+    }
+    
+    @objc func slideshow(_ timer :Timer){
+        ImageView01.image = imglist![self.count]
+        //self.show_count += 1
     }
     
     @IBAction func GoButton(_ sender: Any) {
         if self.count == imglist!.count - 1 {
-            self.count = 0
             ImageView01.image = imglist![self.count]
+            self.count = 0
         }
         else{
-            self.count += 1
             ImageView01.image = imglist![self.count]
+            self.count += 1
         }
     }
     
     @IBAction func BackButton(_ sender: Any) {
         if self.count == 0{
-            self.count = imglist!.count - 1
             ImageView01.image = imglist![self.count]
+            self.count = imglist!.count - 1
         }
         else{
-            self.count -= 1
             ImageView01.image = imglist![self.count]
+            self.count -= 1
         }
     }
     
     @IBAction func PlayAndStopButton(_ sender: UIButton) {
         if sender.titleLabel?.text == "再生"{
-            //anime = UIImage.animatedImage(with: imglist!, duration: 2)
-            //ImageView01.image = anime
             sender.setTitle("停止", for: UIControl.State.normal)
             GoButton.isEnabled = false
             BackButton.isEnabled = false
-            ImageView01.animationImages = imglist!
-            ImageView01.animationRepeatCount = 0
-            ImageView01.animationDuration = 5
-            ImageView01.startAnimating()
-
+            self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(slideshow(_:)), userInfo: nil, repeats: true)
         }
         else if sender.titleLabel?.text == "停止"{
-            //ImageView01.image = nil
             sender.setTitle("再生", for: UIControl.State.normal)
             GoButton.isEnabled = true
             BackButton.isEnabled = true
             ImageView01.stopAnimating()
-            
         }
 
     }
